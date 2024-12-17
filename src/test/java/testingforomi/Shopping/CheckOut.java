840d3ecde68d3b5d9f93174452b74db5b2c485a2
JOSERMAESTRE <jose.rafam00@gmail.com>
2024-12-17 17:00:52 -0500

    new code

--- src/test/java/testingforomi/Shopping/CheckOut.java
+++ src/test/java/testingforomi/Shopping/CheckOut.java
@@ -4,7 +4,6 @@ import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -15,6 +14,7 @@ public class CheckOut extends Base {
 	By TermsCheckLocator = By.id("termsofservice");
 	By CheckOutButtonLocator = By.id("checkout");
 	By GuestButtonLocator = By.xpath("//input[@value='Checkout as Guest']");
+	By SuceesAlertLocator = By.id("bar-notification");
 
 	public CheckOut(WebDriver driver) {
 		super(driver);// TODO Auto-generated constructor stub
@@ -22,10 +22,14 @@ public class CheckOut extends Base {
 
 	public void CheckingOut() {
 		System.out.println(FindElement(LinkShoppingCarLocator).getText());
-		WebDriverWait Wait =   new WebDriverWait(driver,Duration.ofSeconds(10));
-		WebElement linkShoppincar = Wait.until(ExpectedConditions.visibilityOfElementLocated(LinkShoppingCarLocator));
-		click(linkShoppincar);
-		//click(LinkShoppingCarLocator);
+		WebDriverWait Wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+		
+		boolean isElementInvisible = Wait.until(ExpectedConditions.invisibilityOfElementLocated(SuceesAlertLocator));
+        if (isElementInvisible) {
+        	click(LinkShoppingCarLocator);
+        }
+	
+		
 		click(TermsCheckLocator);
 		click(CheckOutButtonLocator);
 		click(GuestButtonLocator);
